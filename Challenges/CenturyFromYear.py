a9dc552862fa344556e6af6be7bfabc4ef4ecea9
Dev <dmb4086@rit.edu>
2021-03-31 19:47:44 -0400

    1st challenge

---
+++ Challenges/CenturyFromYear.py
@@ -0,0 +1,27 @@
+
+"""
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+Example
+
+For year = 1905, the output should be
+centuryFromYear(year) = 20;
+For year = 1700, the output should be
+centuryFromYear(year) = 17.
+"""
+
+
+def centuryFromYear(year):
+    year = str(year)
+    pre = year[:2]
+    post = year[2:]
+
+    if post == "00":
+        century = pre
+    else:
+        century = int(pre) + 1
+
+    return int(century)
+
+
+print(centuryFromYear(1905))
